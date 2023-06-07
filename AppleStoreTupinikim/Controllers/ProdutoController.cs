8467c188be210a588a8f56e9f53f2b7e57baddf6
iErickAraujo <erickaraujo4984@gmail.com>
2023-06-06 21:51:31 -0300

    Adicionar arquivos de projeto.

---
+++ AppleStoreTupinikim/Controllers/ProdutoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppleStoreTupinikim.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Produto()
+        {
+            return View();
+        }
+    }
+}
