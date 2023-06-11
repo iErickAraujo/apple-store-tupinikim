478b3951a351bf0884329ddba512034359153d01
Gbritoo <gb785159@gmail.com>
2023-06-11 13:14:07 -0300

    Adicionando Lista Dos Produtos

---
+++ AppleStoreTupinikim/Controllers/ListaDeProdutosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppleStoreTupinikim.Controllers
+{
+    public class ListadeProdutosController : Controller
+    {
+        public IActionResult ListaDeProdutos()
+        {
+            return View();
+        }
+    }
+}
